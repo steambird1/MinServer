8157e2f417c9ed1c821b7179e81c064cf7f38e2c
steambird1 <steambird@163.com>
2021-12-28 14:28:29 +0800

    (A simple test)

--- testers/Dll1/dllmain.cpp
+++ testers/Dll1/dllmain.cpp
@@ -1,4 +1,4 @@
-﻿// This is a test DLL file for DLL function. Do not try running this.
+﻿// This is a test DLL file for DLL function. Do not try running this (behavior is undefined).
 
 #define _CRT_SECURE_NO_WARNINGS
 #include "../../c_framework.h"
@@ -10,14 +10,17 @@ using namespace std;
 extern "C" {
 #endif
 	__declspec(dllexport) send_info ServerMain(const char *data, sdata *sdata) {
-		static const char *sendup = { "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Type: text/plain\nContent-Length: %d\n\n%s" };
-		char stmp[5000], t[3000];
-		sprintf(t, "MinServer Diag\nTester DLL.dll\n\nValues:\nCurrent Memory Usage State: %.2lf\n\n", sdata->cal_lib.mem_usage());
-		size_t tt = strlen(t);
+		// 301 Moved Permanently
+		static const char *sendup = { "HTTP/1.1 301 Moved Permanently\nContent-Length: %d\n\n%s" };
+		char t[30];
+		char *stmp;
+		sprintf(t, "/cc.html");
+		stmp = (char*)sdata->mc_lib.m_alloc(sizeof(char) *(strlen(t) + strlen(sendup) + 20));
+		/*size_t tt = strlen(t);
 		int i;
 		const int llen = 2000;
 		for (i = 0; i < llen; i++) t[tt + i] = 'A';
-		t[tt + llen] = '\0';
+		t[tt + llen] = '\0';*/
 		sprintf(stmp, sendup, strlen(t), t);
 		/*sprintf(stmp, sendup, strlen(cb), cb);
 		printf("DLL: Outputting returning: \n%s\n", stmp);*/
